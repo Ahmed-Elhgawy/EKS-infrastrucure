0b245664ed2cd951ff04654a69b5ef9051a2edfb
ahmed <ahmadinejad.am50@gmail.com>
2024-02-05 17:54:39 +0200

    update maintf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,40 @@
 module "network" {
   source = "./modules/Network"
 
-  vpc_name            = "Kubernetes_VPC"
-  vpc_cidr            = var.cidr
-  availability_zones  = var.availability_zones
-  public_subnets_cidr = var.public_subnets_cidr
+  vpc_name             = "Kubernetes_VPC"
+  vpc_cidr             = var.cidr
+  availability_zones   = var.availability_zones
+  public_subnets_cidr  = var.public_subnets_cidr
   private_subnets_cidr = var.private_subnets_cidr
-  nat_gw = var.nat_gw
+  nat_gw               = var.nat_gw
+}
+
+module "security" {
+  source = "./modules/Security"
+
+  vpc_id         = module.network.vpc_id
+  db_cidr        = var.cidr
+  rds_creatation = var.rds_creatation
+
+  depends_on = [module.network]
+}
+
+module "database" {
+  count  = var.rds_creatation ? 1 : 0
+  source = "./modules/Database"
+
+  db_name       = var.db_name
+  db_subnets_id = module.network.private_subnets_id
+  db_sg         = [module.security.db_sg]
+  master_user = {
+    username = var.username
+    password = var.password
+  }
+
+  depends_on = [
+    module.network,
+    module.security
+  ]
 }
 
 module "eks" {
@@ -16,19 +44,20 @@ module "eks" {
   cluster_name   = var.cluster_name
   subnets_id     = module.network.public_subnets_id
   instance_types = var.instance_types
-  remote_access = {
-    ssh_key            = var.ssh_key
-  }
   node_size = {
     desired_size = var.node_size.desired_size
     max_size     = var.node_size.max_size
     min_size     = var.node_size.min_size
   }
+  remote_access = {
+    ssh_key = var.ssh_key
+  }
 
   depends_on = [
-    module.network,
+    module.network
   ]
 }
+
 # *********************** GET NODE WORKERS PUBLIC IPS ***********************
 resource "null_resource" "edit_ansibel_inventory" {
   provisioner "local-exec" {
